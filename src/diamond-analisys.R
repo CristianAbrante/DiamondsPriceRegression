ede1172356791086c8729d7d136553e0285485dc
agonzalezl <angel.gonzalez.lpz@gmail.com>
2019-12-03 20:27:19 +0100

    añadidos comentarios en codigo

--- src/diamond-analisys.R
+++ src/diamond-analisys.R
@@ -13,11 +13,21 @@ if (!require("dplyr"))
   install.packages("dplyr")
 if (!require("ElemStatLearn"))
   install.packages("car")
+if (!require("lmtest"))
+  install.packages("lmtest")
+if (!require("normtest"))
+  install.packages("normtest")
+if (!require("tseries"))
+  install.packages("tseries")
 
 # Load libraries
 library(ggplot2)
 library(dplyr)
 library(car)
+library(lmtest)
+library(normtest)
+library(tseries)
+
 
 # Load data
 # You have to set the current directory in order to load the data.
@@ -98,12 +108,37 @@ diamonds$caratageCategorical <- factor(diamonds$caratageCategorical, levels = c(
 update1=update(Full,.~.+caratage*caratageCategorical, diamonds)
 summary(update1)
 
+# Is this regression model satisfactory? Are the standard assumptions of linear
+# regression validated? Are the numerical estimates sensible?
+dwtest( update1, alternative="two.sided") # for independence
+jarque.bera.test(residuals(update1)) # for normality
+bptest(update1) # Variance
+
+# Interpret the interaction parameter med*carat. What can we infer on the
+# incremental pricing of caratage in the 3 clusters?
+## caratage:caratageCategoricalmedium  3672.18     896.59   4.096 5.46e-05 ***
+# For small diamonds, the incrising of caratage increments 884.554 each 0.1 of caratage
+# For medium diamonds, the incrising of caratage increments 367.218 each 0.1 of caratage
+# For large diamonds, the incrising of caratage increments -7606.99 each 1 of caratage
 
 # Which is more highly valued: colour or clarity? 
-# purityD                             3180.57
-# clarityIF                           1751.03
+## purityD                             3180.57
+## clarityIF                           1751.03
 
-#3.b Include the square of carat as a new explanatory variable. It avoids the subjectivity of clusters denition.
+# All other things being equal, what is the average price diference between a
+# grade D diamond and another one graded (a) I (b) E?
+##purityD                             3180.57
+##(Intercept) -> I                    -3265.59
+##purityE                             1932.54
+
+# All other things being equal, are there price diferences amongst the stones
+# appraised by the GIA, IGI and HRD?
+##certificateGIA                       15.21
+##certificateIGI                       -397.34
+##(Intercept) -> HRD                   -3265.59
+
+
+#3.b Include the square of carat as a new explanatory variable. It avoids the subjectivity of clusters definition.
 update2=update(Full,.~.+I(caratage^2), diamonds)
 summary(update2)
 
@@ -111,6 +146,13 @@ summary(update2)
 # Question 4
 ##########################################################################
 
+#Which of the two remedial actions do you prefer and why? Think on terms of interpretability and validity of the assumptions.
 anova(update1,update2)
+
+# We prefer update2
+# Interpretability: It has more sense and is easier interpretable, that price increments exponentialy with the caratage than 
+# that adding the categorical version of caratage (when we already have the caratage) has repercusion in the price.
+# Validity : The P value is 1.162e-08 ***
+
 
 
